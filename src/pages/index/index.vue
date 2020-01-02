bbb3156bf820d45d41834924d325086f0a572fde
bimocanquan <1457766258@qq.com>
2020-01-02 23:11:00 +0800

    second

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,34 +1,33 @@
 <template>
   <div>
+    <swiper 
+        :indicator-dots="indicatorDots"
+        :autoplay="autoplay"
+        :interval="interval" 
+        :duration="duration"
+      >
+        <block v-for="img in imgurls" :key="img">
+          <swiper-item>
+            <image :src="img" style="width=100%"/>
+          </swiper-item>
+        </block>
+      </swiper>
     <i-grid i-class="no-border">
-    <i-grid-item i-class="no-border">
+    <i-grid-item @click="gotype(grid)" v-for="grid in grids" :key="grid" i-class="no-border">
         <i-grid-icon>
-            <image src="/static/images/1.png" />
+            <image :src="grid.image" />
         </i-grid-icon>
-        <i-grid-label>经典文学</i-grid-label>
-    </i-grid-item>
-     <i-grid-item i-class="no-border">
-        <i-grid-icon>
-            <image src="/static/images/2.png"/>
-        </i-grid-icon>
-        <i-grid-label>推理悬疑</i-grid-label>
-    </i-grid-item>
-     <i-grid-item i-class="no-border">
-        <i-grid-icon>
-            <image src="/static/images/3.png" />
-        </i-grid-icon>
-        <i-grid-label>都市言情</i-grid-label>
-    </i-grid-item>
-     <i-grid-item i-class="no-border">
-        <i-grid-icon>
-            <image src="/static/images/4.png" />
-        </i-grid-icon>
-        <i-grid-label>外国著作</i-grid-label>
+        <i-grid-label>{{grid.title}}</i-grid-label>
     </i-grid-item>
     </i-grid>
-    <i-panel title="热门">
-    <view style="padding: 15px;">PANEL 内容区域</view>
-</i-panel>
+    <i-panel :title="title_name">
+     <view style="padding: 15px;">     
+       <i-card v-for="item in top" :key="item" i-class="split" :title="item.name" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+         <view slot="content">{{item.remark}}</view>
+         <view slot="footer">{{item.author}}</view>
+        </i-card>    
+     </view>
+    </i-panel>
   </div>
   </template>
 
@@ -36,13 +35,39 @@
 export default {
   data () {
     return {
-      
+      title_name:"热门",
+      grids:[
+        {title:"经典文学",image:"/static/images/1.png"},
+        {title:"推理悬疑",image:"/static/images/2.png"},
+        {title:"都市言情",image:"/static/images/3.png"},
+        {title:"外国著作",image:"/static/images/4.png"}
+      ],
+      top:[
+        {name:"书名1",author:"作者1",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍1"},
+        {name:"书名2",author:"作者2",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍2"},
+        {name:"书名3",author:"作者3",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍3"},
+        {name:"书名4",author:"作者4",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍4"}
+
+      ],
+      imgUrls: [
+        'http://www.canyin88.com/uploads/image/2019/04/16/1555378220872933.jpg',
+        'http://www.canyin88.com/uploads/image/2019/04/15/1555321255843942.jpg',
+        'http://www.canyin88.com/uploads/190415/99ad8154e7332ca96ccb323580b3b8a2_3.jpg'
+      ],
+      indicatorDots: true,
+      autoplay: true,
+      interval: 5000,
+      duration: 1000,
       }
   },
 
 
   methods: {
-  
+    gotype(){
+      console.log(type)
+      let url ='../list/main?type' +type.title
+      mpvue.navigateTo({url})
+    }
     },
  
 
--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+     <view class="userinfo">
+    <view class="userinfo-avatar">
+    <open-data type="userAvatarUrl"></open-data>
+    </view>
+    <open-data type="userNickName"></open-data>
+    </view>
   </div>
   </template>
 
@@ -24,4 +30,16 @@ export default {
 </script>
 
 <style scoped>
+.userinfo-avatar {
+  overflow:hidden;
+  display: block;
+  width: 160rpx;
+  height: 160rpx;
+  margin: 20rpx;
+  margin-top: 50rpx;
+  border-radius: 50%;
+  border: 2px solid #fff;
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+}
+
 </style>
